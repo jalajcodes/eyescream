bf8057db25090fe8ac83ada1682b50f2c1a0c7a4
Jalaj <Jalaj@mail.com>
2020-07-05 16:09:29 +0530

    added tags page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+	storage: multer.memoryStorage(),
+	fileFilter(req, file, next) {
+		const isPhoto = file.mimetype.startsWith('image/');
+		if (isPhoto) {
+			next(null, true);
+		} else {
+			next({ message: "That file type isn't allowed" }, false);
+		}
+	},
+};
 
 exports.homePage = (req, res) => {
 	res.render('index', {
@@ -11,6 +26,26 @@ exports.addStore = (req, res) => {
 	res.render('editStore', {
 		title: 'Add Store',
 	});
+};
+
+exports.upload = multer(multerOptions).single('photo');
+exports.resize = async (req, res, next) => {
+	//check if there is no new file
+	if (!req.file) {
+		next(); //skip to next middleware
+	} else {
+		//get extension
+		const extension = req.file.mimetype.split('/')[1];
+		//create photo on req.body so that createStore can add it in store
+		req.body.photo = `${uuid.v4()}.${extension}`;
+		//now we resize (jimp is based on promises, so we can use await)
+		const photo = await jimp.read(req.file.buffer);
+		await photo.resize(800, jimp.AUTO);
+		await photo.write(`./public/uploads/${req.body.photo}`);
+
+		// after writing the photo to filesystem. Keep going!
+		next();
+	}
 };
 
 exports.createStore = async (req, res) => {
@@ -40,7 +75,10 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
-	// set the location data to Point
+	// set the location data to Point becaue our model has a type of point as location. If we don't add this here
+	// then when we update the store it will not be added to our location schema type because mongodb didn't add
+	// default values when updating. This type of point is necessaary because we want to get store close to each other.
+
 	req.body.location.type = 'Point';
 
 	// find and update the store
@@ -52,7 +90,19 @@ exports.updateStore = async (req, res) => {
 	// tell them it worked and redirect them to the store
 	req.flash(
 		'success',
-		`Successfully updated <strong>${store.name}.</strong> <a href="/stores/${store.slug}">View Store &rarr; </a>`
+		`Successfully updated <strong>${store.name}.</strong> <a href="/store/${store.slug}">View Store &rarr; </a>`
 	);
 	res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+	const store = await Store.findOne({ slug: req.params.slug });
+	if (!store) return next();
+	res.render('store', { store, title: store.name });
+};
+
+exports.getStoresByTag = async (req, res, next) => {
+	const tags = await Store.getTagsList();
+	const tag = req.params.tag;
+	res.render('tags', { title: 'Tags', tags, tag });
 };
--- models/Store.js
+++ models/Store.js
@@ -34,16 +34,32 @@ const storeSchema = new mongoose.Schema({
 			required: "Address field can't be blank.",
 		},
 	},
+	photo: String,
 });
 
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', async function (next) {
 	// this.isModified('name') ? (this.slug = slug(this.name)) : next();
 	if (!this.isModified('name')) {
 		next(); // skip it
 		return; // stop this function from running
 	}
+
 	this.slug = slug(this.name);
+	// find other stores that have a slug of 'store', 'store-1', etc.. //
+	const slugRegex = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+	const storesWithSlug = await this.constructor.find({ slug: slugRegex });
+	if (storesWithSlug.length) {
+		this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+	}
 	next();
 });
+
+storeSchema.statics.getTagsList = function () {
+	return this.aggregate([
+		{ $unwind: '$tags' },
+		{ $group: { _id: '$tags', count: { $sum: 1 } } },
+		{ $sort: { count: -1 } },
+	]);
+};
 
 module.exports = mongoose.model('Store', storeSchema);
